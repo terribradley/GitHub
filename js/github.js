ebaec8aa9167c95f5dc523fd8e5116edabd88f65
Terri Bradley <terribradley3@gmail.com>
2016-08-05 09:05:27 -0700

    test

--- js/github.js
+++ js/github.js
@@ -6,7 +6,9 @@ function GitRepos() {
 
 GitRepos.prototype.getRepos = function(name) {
   $.get('https://api.github.com/users/' + name + '/repos?access_token=' + apiKey).then(function(response){
-      $('#output').text("Here is a list of" + name + "'s Repositories. Included is a title and a brief description:"+ response[2].name);
+    for (i=0 i<response.length; i++) {
+      $('#output').text("Here is a list of" + name + "'s Repositories. Included is a title and a brief description:"+ response[i].name + response[i].description);  
+    }
       // console.log(response[1].name);
       // console.log(response[1].description);
   }).fail(function(error){
