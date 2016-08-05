b369a8dd70bc581444f280650599ecaf440a7270
Terri Bradley <terribradley3@gmail.com>
2016-08-05 09:56:09 -0700

    added a for loop to cycle through all repo names and descriptions

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,18 +1,5 @@
 var GitRepos = require('./../js/github.js').gitreposModule;
 
-// var displayGitHubUser = function(ghUser) {
-//   $('.show-gh-user-info').text("The GitHub username you entered is " + ghUser + ".");
-// }
-//
-// $(document).ready(function() {
-//   var currentGitHubObject = new GitHub();
-//   $('#gh-username-button').click(function() {
-//     var ghUser = $('#gh-username-input').val();
-//     $('#gh-username-input').val("");
-//     currentGitHubObject.getRepos(ghUser, displayGitHubUser);
-//     // currentGitHubObject.getGitHub(ghUser, displayGitHubUser);
-//   });
-// });
 
 $(document).ready(function() {
   var currentGitRepos = new GitRepos();
--- js/github.js
+++ js/github.js
@@ -6,13 +6,15 @@ function GitRepos() {
 
 GitRepos.prototype.getRepos = function(name) {
   $.get('https://api.github.com/users/' + name + '/repos?access_token=' + apiKey).then(function(response){
-    for (i=0 i<response.length; i++) {
-      $('#output').text("Here is a list of" + name + "'s Repositories. Included is a title and a brief description:"+ response[i].name + response[i].description);  
-    }
-      // console.log(response[1].name);
+
+    // console.log(response);
+    // for(var i=0; i<response.length; i++) {
+      $('#output').text("Here is a list of" + name + "'s Repositories. Included is a title and a brief description:");
+      $('repos-list').append('<li>'+ response[1].name + '</li>' + '<li>' response[1].description + '<li>');
+    });
       // console.log(response[1].description);
-  }).fail(function(error){
-    $('.output').text(error.responseJSON.message);
+  });.fail(function(error){
+    $('.error-message').text(error.responseJSON.message);
   });
 };
 
